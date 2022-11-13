79f82d5aa094c995f28c4e30ff5fae81e933864c
badushaebrahim <bbadusha13@gmail.com>
2022-11-13 16:08:09 +0530

    working swagger but data from application.yml fail

--- mock/mock/src/main/java/training/acabes/mock/MockApplication.java
+++ mock/mock/src/main/java/training/acabes/mock/MockApplication.java
@@ -2,6 +2,8 @@ package training.acabes.mock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MockApplication {
@@ -9,5 +11,7 @@ public class MockApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MockApplication.class, args);
 	}
+
+
 
 }
--- mock/mock/src/main/java/training/acabes/mock/controller/StudentController.java
+++ mock/mock/src/main/java/training/acabes/mock/controller/StudentController.java
@@ -1,9 +1,10 @@
 package training.acabes.mock.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import training.acabes.mock.model.ResponceModel;
@@ -19,9 +20,11 @@ public class StudentController {
 
     @Autowired
     StudentService studentService;
-
+    Logger log = LoggerFactory.getLogger(StudentController.class); //logingn class slf4j
     @PostMapping
     public ResponseEntity<Student> postStudent(@RequestBody Student student){
+
+
         Student data= studentService.create(student);
         return new ResponseEntity<>(data, HttpStatus.CREATED);
     }
@@ -29,6 +32,11 @@ public class StudentController {
     @GetMapping
     public ResponseEntity<Student> getStudent(@RequestParam String name,@RequestParam int age,@RequestParam Date dob
                                               ,@RequestParam Long id){
+        log.trace("get called");//while in trace
+        log.info("get info");//at normal log
+        log.debug("get debug");//whith debug true only
+        log.warn("get warn");//every where
+        log.error("get error");//every where
         Student data= studentService.get(name,age,dob,id);
         return new ResponseEntity<>(data, HttpStatus.OK);
     }
