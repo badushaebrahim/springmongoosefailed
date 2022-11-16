3164c37ab619ea962669a7ee494cc06aa31ddcf6
badushaebrahim <bbadusha13@gmail.com>
2022-11-16 14:50:41 +0530

    working postgress

---
+++ postgg/postgg/src/main/java/com/acab/postgg/model/entity/responce/ResponceModel.java
@@ -0,0 +1,12 @@
+package com.acab.postgg.model.entity.responce;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponceModel {
+    private String msg;
+}
